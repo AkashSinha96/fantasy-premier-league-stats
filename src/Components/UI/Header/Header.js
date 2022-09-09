3191b0499e48ce99db0b7b19020a13d9de1aac75
Akash Sinha <akashsinha0807@outlook.com>
2022-09-09 13:37:49 +0530

    changed

--- src/Components/UI/Header/Header.js
+++ src/Components/UI/Header/Header.js
@@ -2,7 +2,7 @@ const Header = (props) => {
   return (
     <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800">
       <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com/" class="flex items-center">
+        <a href="https://fpl-stats-app.herokuapp.com/" class="flex items-center">
           <img
             src="https://flowbite.com/docs/images/logo.svg"
             class="mr-3 h-6 sm:h-9"
